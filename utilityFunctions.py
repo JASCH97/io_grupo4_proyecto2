2b152edc8931943ac6ab7b95c3f9e4df1c7d1f27
JASCH97 <joansc16@outlook.com>
2022-04-28 08:04:14 -0600

    Se elimina p1.txt que se resubió por error y se agregan las funciones encargadas de leer la info del archivo

---
+++ utilityFunctions.py
@@ -0,0 +1,55 @@
+import sys
+
+#Global variables
+fileInformation = []
+
+"""
+Function: readInputFile
+Input: -
+Output: -
+Description: Function that read the information from input file and call saveInformationFromFile function
+             to convert and save the strings informtation in a list with integers
+"""
+def readInputFile():
+
+    fileName = sys.argv[2]
+    informationInStr = []
+
+    with open("./Problems/" + fileName) as file:
+        lines = file.readlines()
+
+        for line in lines:
+            informationInStr.append(line.strip('\n'))
+
+    convertInformationFromFile(informationInStr)
+
+"""
+Function: convertInformationFromFile
+Input: -
+Output: -
+Description: Function that converts the information in str format to int format and save it in a list
+"""
+def convertInformationFromFile(information):
+    i = 0
+
+    while i <= len(information) -1:
+
+        if len(information[i].split(",")) > 1:
+
+            numbersInString = []
+            
+            for number in information[i].split(","):
+                numbersInString.append(int(number))
+
+            i += 1
+            fileInformation.append(numbersInString)
+        
+        else:
+            fileInformation.append(int(information[i].split(",")[0]))
+            i+=1
+
+
+
+
+#readInputFile()
+#print(fileInformation)

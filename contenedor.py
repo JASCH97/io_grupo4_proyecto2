ba67daf86ec3687ca2ad35b4cdc7d9eef1acc071
Newconker <66338359+Newconker@users.noreply.github.com>
2022-05-13 15:44:32 -0600

    incluido fuerzabruta + comentarios
    funcional 90%

--- contenedor.py
+++ contenedor.py
@@ -1,16 +1,41 @@
 from timeit import default_timer
+from utilityFunctions import *
 
 
-W=30
-data=[[5,20],[15,50],[10,60],[10,62],[8,40]]
+W=0
+data=[]
 Fmatriz=[]
-inContainer=[]
-inContainer2=()
+inContainer=[] #save results of the dynamic method
+inContainer2=()#save results of brute force method
+
+#read the file
+def readtext():
+    global W,data
+    readInputFile()
+    W=fileInformation[0]
+    data=fileInformation[1:]
+    
+
+"""
+Function: matrix
+Input: -
+Output: -
+Description: generates the necessary matrix filled with 0 
+             to be able to work the dynamic method.
+"""
 def  matrix():
     for i in range(len(data)+1):
         a = [0]*(W+1)
         Fmatriz.append(a)
         
+
+
+"""
+Function: bottomUpContainer
+Input: -
+Output: -
+Description: iterates through the array filling it with the data from data .
+"""
 #data[i-1][0] = wi
 #data[i-1][1] = bi
 
@@ -27,6 +52,14 @@ def bottomUpContainer():
                     else:
                         Fmatriz[i][w] = Fmatriz[i-1][w]
 
+"""
+Function:  findElementds
+Input: -
+Output: -
+Description:traverse the matrix back to front
+            getting the correct data and saving it in inContainer
+"""
+
 def findElementds():
     i = len(Fmatriz)-1
    
@@ -42,28 +75,42 @@ def findElementds():
                 k=0
         else:
             i = i-1
+"""
+Function:  finalresults
+Input: -
+Output: -
+Description: prints the final results of the dynamic method
+
+"""
 
 def finalresults():
     maximumprofit = 0
     for i in list(inContainer):
         maximumprofit= maximumprofit + data[i-1][1]
     
-    print("Beneficio máximo: ",maximumprofit)
-    print("Incluidos: ", end="")
+    print("maximum benefit: ",maximumprofit)
+    print("included: ", end="")
 
     for e in inContainer[:len(inContainer)-1]:
         print(e, end=", ")
     print(inContainer[len(inContainer)-1])
 
+"""
+Function:  BruteForceContainer
+Input: -
+Output: brute force result
+Description: recursive method that obtains the maximum number of in brute force,
+             in addition it is extracting which are the articles that were used to obtain that result
 
+"""
 def BruteForceContainer(W, data, n, inContainer2):
-   # initial conditions
+   
     if n == 0 or W == 0 :
        return 0,inContainer2
-   # If weight is higher than capacity then it is not included
+ 
     if (data[n-1][0] > W):
         return BruteForceContainer(W, data, n-1, inContainer2)
-   # return either nth item being included or not
+   
     else:
         inContainer2Aux = (inContainer2 +(n,))
         temp,inContainer2Aux =  BruteForceContainer(W-data[n-1][0], data, n-1, (inContainer2Aux))
@@ -73,24 +120,37 @@ def BruteForceContainer(W, data, n, inContainer2):
             return temp,inContainer2Aux
         return temp2,inContainer2
   
-       
 
-def main(x) :
+"""
+Function:  main
+Input: -
+Output: -
+Description: it is to be able to use the functions described above,
+             and show the results
+""" 
+
+def main() :
+    readtext()
+    x=algorithmToUse[0]
+   
     if x == 1:
+        print("brute force")
         n=len(data)
         w=W
         start = default_timer()
         finalresults2 = BruteForceContainer(w, data, n, inContainer2)
-        print("Beneficio máximo: ", finalresults2[0])
+        #prints the final results of the brute force method
+        print("maximum benefit: ", finalresults2[0])
         
-        print("Incluidos: ", end="")
+        print("included: ", end="")
         for e in finalresults2[1][:len(finalresults2[1])-1]:
             print(e, end=", ")
         print(finalresults2[1][len(finalresults2[1])-1])
         end = default_timer()
         executionTime = end - start
         print("Execution Time: ",executionTime, "seconds\n")
-    else:
+    elif x == 2:
+        print("dynamic")
         start = default_timer()
         matrix()
         bottomUpContainer()
@@ -99,12 +159,13 @@ def main(x) :
         end = default_timer()
         executionTime = end - start
         print("Execution Time: ",executionTime, "seconds\n")
+    else:
+        print("wrong method entered: 1 brute force method, 2 dynamic method")
 
           
+main()
 
 
-
-main(1)
 
 
  

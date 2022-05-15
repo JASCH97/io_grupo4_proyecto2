0bbc8d4edef6da42d9160e047e73130011f85e0b
Esteban Aguilera <luisagui1997@gmail.com>
2022-05-15 03:23:32 -0600

    BlockTower To Main

---
+++ bloques.py
@@ -0,0 +1,80 @@
+import sys
+import utilityFunctions
+
+# Frente (F), Profundidad (P) y Altura (A) donde F <= P
+# Los bloques se pueden rotar en sus dimensiones, y se pueden usar más de un bloque según
+# las rotaciones del mismo tipo para formar la torre. Cada bloque tiene 3 posibles rotaciones.
+# Manteniendo siempre el Frente menor o igual que la Profundidad. Ejemplo bloque
+# {FxPxA} de {2x3x1} solo puede tener 3 posiciones {2x3x1}, {1x2x3} y {1x3x2}.
+
+
+
+def blockTowerBruteForce(BlockList):
+        print("Fuerza Bruta")
+        print("Input:", arg2)
+        utilityFunctions.printBlocks(BlockList)
+        BlocksToPerm = []
+        for Block in BlockList:
+            #Block = BlockList[4]
+            BlockChildren = utilityFunctions.ReturnThreeCandidates(Block)
+            for block in BlockChildren:
+                BlocksToPerm.append(block)
+
+        print(BlocksToPerm)
+
+        Permutations = utilityFunctions.PermuteBlocks(BlocksToPerm)
+        OptimalCandidate = None
+        OptimalHeight = 0
+        for perm in Permutations:
+            permAux = perm.copy()
+            Outcome = utilityFunctions.GetOptimalHeight(perm,arg1)
+         
+            Height = Outcome[0]
+            NewOrder = Outcome[1]
+            if Height > OptimalHeight:
+                OptimalCandidate = NewOrder
+                
+                OptimalHeight = Height
+
+        print("Blocks:",OptimalCandidate)
+        print("Max Height:",OptimalHeight)
+     
+
+def blockTowerDynamic(BlockList):
+    print("Dinamico")
+    print("Input:",arg2)
+    utilityFunctions.printBlocks(BlockList)
+
+    Blocks = []
+    for Block in BlockList:
+        BlockChildren = utilityFunctions.RotateDynamic(Block)
+        for BlockAux in BlockChildren:
+            Blocks.append(BlockAux)
+
+    utilityFunctions.GetOptimalHeight(Blocks,arg1)
+
+
+# save the first argument (second position in argv list) to a variable
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+a_file = open(arg2, "r")
+
+list_of_lists = []
+for line in a_file:
+  stripped_line = line.strip().replace(",","")
+  line_list = stripped_line.split()
+  list_of_lists.append(line_list)
+
+a_file.close()
+
+for i in range(len(list_of_lists)):
+    for j in range(len(list_of_lists[i])):
+        list_of_lists[i][j] = int(list_of_lists[i][j])
+
+if arg1 == "1":
+    blockTowerBruteForce(list_of_lists)
+elif arg1 == "2":
+    blockTowerDynamic(list_of_lists)
+else:
+    print("Choose an algorythm")
--- utilityFunctions.py
+++ utilityFunctions.py
@@ -1,4 +1,6 @@
 import sys
+import random
+import itertools
 
 #Global variables
 fileInformation = []
@@ -51,7 +53,161 @@ def convertInformationFromFile(information):
             fileInformation.append(int(information[i].split(",")[0]))
             i+=1
 
+"""
+Utils for Brute Force Algorithm
+"""
+def RotateBlockBrute(Block):
+    for i in range(len(Block)):
+            # Obtener un índice aleatorio
+            randindex = random.randint(0, len(Block) - 1)
+            # Intercambiar
+            temp = Block[i]
+            Block[i] = Block[randindex]
+            Block[randindex] = temp
 
+    return Block
+
+
+
+def ReturnThreeCandidates(Block):
+    candidates = []
+
+    inp_list = Block
+    permutations = list(itertools.permutations(inp_list))
+
+    for block in permutations:
+        if ValidateBlockBrute(block):
+            candidates.append(list(block))
+
+    newPermutation = []
+    for block in candidates:
+        if block not in newPermutation:
+            newPermutation.append(block)
+
+    return newPermutation
+
+def PermuteBlocks(BlockList):
+    inp_list = BlockList
+    permutations = list(itertools.permutations(inp_list))
+
+    ListedPermutations = []
+    for block in permutations:
+        if ValidateBlockBrute(block):
+            ListedPermutations.append(list(block))
+
+    return ListedPermutations
+
+
+def ValidateBlockBrute(Block):
+
+    F = Block[0]
+    P = Block[1]
+    A = Block[2]
+
+    if(F<=P):
+        return True
+    return False
+
+"""
+Utils for Dynamic Algorithm
+"""
+def RotateDynamic(Block):
+    F = Block[0]
+    P = Block[1]
+    A = Block[2]
+
+    BlockZero = []
+    BlockOne = []
+    BlockTwo = []
+    BlockThree = []
+    BlockFour = []
+    BlockFive = []
+
+
+    if A<=F and A<=P:
+        BlockZero.append(A)
+        BlockZero.append(P)
+        BlockZero.append(F)
+    if A<=P:
+        BlockOne.append(A)
+        BlockOne.append(P)
+        BlockOne.append(F)
+    if P>=A:
+        BlockTwo.append(A)
+        BlockTwo.append(F)
+        BlockTwo.append(P)
+    if A>=F:
+        BlockThree.append(F)
+        BlockThree.append(A)
+        BlockThree.append(P)
+    if P<=A:
+        BlockFour.append(P)
+        BlockFour.append(A)
+        BlockFour.append(F)
+    if P<=F:
+        BlockFive.append(P)
+        BlockFive.append(F)
+        BlockFive.append(A)
+
+    candidates = []
+    candidates.append(Block)
+
+    if BlockOne!=[]:
+        candidates.append(BlockOne)
+    if BlockTwo != []:
+        candidates.append(BlockTwo)
+    if BlockThree != []:
+        candidates.append(BlockThree)
+    return candidates
+
+def GetOptimalHeight(BlockList,Arg):
+    NewOrder = []
+
+    #for count in range (0,len(BlockList)-1):
+    while BlockList!=[]:
+        BaseIndex = 0
+        BlockBase = [0, 0, 0]
+        x = 0
+        while x <= len(BlockList)-1:
+                #print(BlockBase)
+
+                if BlockList[x][0]>=BlockBase[0] and BlockList[x][1]>=BlockBase[1]:
+                    #print(BlockList[x][0],BlockList[x][1])
+                    BlockBase = BlockList[x]
+                    BaseIndex = x
+                x=x+1
+
+        NewOrder.append(BlockBase)
+        BlockList.pop(BaseIndex)
+        for y in BlockList:
+            if y == BlockBase:
+                NewOrder.append(BlockBase)
+                BlockList.remove(y)
+
+    #print("Blocks:", NewOrder)
+    aux = 1
+    while aux <= len(NewOrder)-1:
+        #print(aux)
+        if (NewOrder[aux][0]>NewOrder[aux-1][0]) or (NewOrder[aux][1]>NewOrder[aux-1][1]):
+            NewOrder.pop(aux)
+        else:
+            aux = aux + 1
+
+    height = 0
+    for block in NewOrder:
+        height = height + block[2]
+
+    if Arg == "2":
+        #print("BlockList:", BlockList)
+        print("Blocks:", NewOrder)
+        print("Max Height:", height)
+
+    outcome = [height,NewOrder]
+    return outcome
+
+def printBlocks(BlockList):
+    for block in BlockList:
+        print(block)
 
 
 #readInputFile()

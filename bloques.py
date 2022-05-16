07ccad868551b151535ebf249508a9b51426f122
Esteban Aguilera <luisagui1997@gmail.com>
2022-05-16 11:37:46 -0600

    Internal Doc For BlockTowers

--- bloques.py
+++ bloques.py
@@ -1,13 +1,14 @@
 import sys
 import utilityFunctions
 
-# Frente (F), Profundidad (P) y Altura (A) donde F <= P
-# Los bloques se pueden rotar en sus dimensiones, y se pueden usar más de un bloque según
-# las rotaciones del mismo tipo para formar la torre. Cada bloque tiene 3 posibles rotaciones.
-# Manteniendo siempre el Frente menor o igual que la Profundidad. Ejemplo bloque
-# {FxPxA} de {2x3x1} solo puede tener 3 posiciones {2x3x1}, {1x2x3} y {1x3x2}.
-
-
+"""
+Function: blockTowerBruteForce
+Input: BlockList
+Output: -
+Description: This function is responsible of receiving the Blocks initially set in order 
+to rotate them and call its respective functions to rearranged them in 
+order to the get the maximun value of the fittest candidate, which is going to be the Output
+"""
 
 def blockTowerBruteForce(BlockList):
         print("Fuerza Bruta")
@@ -38,7 +39,15 @@ def blockTowerBruteForce(BlockList):
 
         print("Blocks:",OptimalCandidate)
         print("Max Height:",OptimalHeight)
-     
+
+"""
+Function: blockTowerDynamic
+Input: BlockList
+Output: -
+Description: This function is responsible of receiving the Blocks initially set in order 
+to rotate them and call its respective functions to rearranged them in 
+order to the get the maximun value of the fittest candidate by validating the base cases, which is going to be the Output
+"""
 
 def blockTowerDynamic(BlockList):
     print("Dinamico")
@@ -54,12 +63,17 @@ def blockTowerDynamic(BlockList):
     utilityFunctions.GetOptimalHeight(Blocks,arg1)
 
 
-# save the first argument (second position in argv list) to a variable
+"""
+save the first argument (second position in argv list) to a variable
+"""
 arg1 = sys.argv[1]
 arg2 = sys.argv[2]
 
 a_file = open(arg2, "r")
 
+"""
+Returns the .txt file in a list
+"""
 list_of_lists = []
 for line in a_file:
   stripped_line = line.strip().replace(",","")
@@ -71,7 +85,10 @@ a_file.close()
 for i in range(len(list_of_lists)):
     for j in range(len(list_of_lists[i])):
         list_of_lists[i][j] = int(list_of_lists[i][j])
-
+        
+"""
+Choose Algorythm in the terminal
+"""
 if arg1 == "1":
     blockTowerBruteForce(list_of_lists)
 elif arg1 == "2":
